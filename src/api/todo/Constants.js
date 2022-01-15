44b398d04a859f7ab035c06f2ae792bd4fdec21d
praveen444 <praveen.mundlapati@gmail.com>
2022-01-15 12:34:33 +0530

    Consts added

--- src/api/todo/Constants.js
+++ src/api/todo/Constants.js
@@ -1,3 +1,4 @@
 export const API_URL = "http://localhost:8080"
 export const JPA_API_URL = "http://localhost:8080/jpa"
 export const JPA_API_URL2 = "http://localhost:8080/jpa"
+export const JPA_API_URL3 = "http://localhost:8080/jpa"
